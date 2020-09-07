6ab456ab94bd77447c8c91a3bbf9fd2633ab2f5e
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2020-09-07 23:51:32 +1000

    Changed on_ready to display more relevant info

--- bot.py
+++ bot.py
@@ -32,18 +32,15 @@ def main():
 
     @bot.event
     async def on_ready():
-        guild = discord.utils.get(bot.guilds, name=GUILD)
-        # for guild in bot.guilds:
-        #     if guild.name == GUILD:
-        #         break
+        # Get guild with matching name
+        # guild = discord.utils.get(bot.guilds, name=GUILD)
 
-        print(
-            f'{bot.user.name} is connected to the following guild:\n'
-            f'{guild.name}(id: {guild.id})\n'
-        )
-
-        members = '\n - '.join([member.name for member in guild.members])
-        print(f'Guild Members:\n - {members}')
+        for guild in bot.guilds:
+            print(
+                f'{bot.user.name} is connected to the following guild:\n'
+                f'{guild.name}(id: {guild.id})\n'
+                f'Guild Size: {len(guild.members)}\n'
+            )
 
     @bot.event
     async def on_member_join(member):
